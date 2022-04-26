9375373e3636d4fc42d6fd2b5cd81e0a3bfbfc73
jmurphy5613 <jmurphy5613@gmail.com>
2022-04-26 16:39:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root">
+      <h1 className="main-title">Set the Fordle word</h1>
+      <input placeholder="5 letter word"/>
     </div>
   );
 }
