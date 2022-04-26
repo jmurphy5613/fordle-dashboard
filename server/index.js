727901214223947c772690d0f6d74e03e3fc9e2a
John Murphy <jmurphy5613@gmail.com>
2022-04-26 16:49:11 -0400

    set up proper folder structure, created express server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
